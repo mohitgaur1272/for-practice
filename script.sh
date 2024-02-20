991723370d9068f5eec1674ab0423151026582da
mohitgaur1272 <138590767+mohitgaur1272@users.noreply.github.com>
2024-02-20 15:55:10 +0530

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,35 +1,46 @@
 #!/bin/bash
 
 sudo touch /tmp/install_apache_docker_jenkins.sh
+
 echo '#!/bin/bash
+
 # Update package list
 sudo apt-get update
+
 # Install Apache2
 sudo apt-get install -y apache2
+
 # Start Apache2 service and enable it to start on boot
 sudo systemctl start apache2
 sudo systemctl enable apache2
+
 # Install Docker
 sudo apt-get install -y docker.io
+
 # Adjust permissions for Docker socket
 sudo chmod 777 /var/run/docker.sock
+
 # Add user to the docker group
 sudo usermod -aG docker ubuntu
+
 # Install OpenJDK 11
 sudo apt update -y
 sudo apt install openjdk-11-jdk -y
+
 # Add Jenkins GPG key
 wget -q -O - http://pkg.jenkins-ci.org/debian/jenkins-ci.org.key | sudo apt-key add -
+
+# Add Jenkins repository
 echo deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] \
   https://pkg.jenkins.io/debian-stable binary/ | sudo tee \
   /etc/apt/sources.list.d/jenkins.list > /dev/null
-#sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 5BA31D57EF5975CA
-# Add Jenkins repository
-#echo "deb [signed-by=/usr/share/keyrings/jenkins-keyring.gpg] https://pkg.jenkins.io/debian-stable binary/" | sudo tee /etc/apt/sources.list.d/jenkins.list > /dev/null
+
 # Update package list
 sudo apt update -y
+
 # Install Jenkins
 sudo apt install jenkins -y
+
 # Start Jenkins service and enable it to start on boot
 sudo systemctl start jenkins
 sudo systemctl enable jenkins' > /tmp/install_apache_docker_jenkins.sh

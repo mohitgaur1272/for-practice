5a6bc3d8c16546d9101947869fb056bab5d7689c
mohitgaur1272 <138590767+mohitgaur1272@users.noreply.github.com>
2024-02-20 16:10:27 +0530

    Update script.sh

--- script.sh
+++ script.sh
@@ -27,13 +27,13 @@ sudo usermod -aG docker ubuntu
 sudo apt update -y
 sudo apt install openjdk-11-jdk -y
 
-# Add Jenkins GPG key
-wget -q -O - http://pkg.jenkins-ci.org/debian/jenkins-ci.org.key | sudo apt-key add -
-
-# Add Jenkins repository
+# Add Jenkins GPG key ,# Add Jenkins repository
+curl -fsSL https://pkg.jenkins.io/debian-stable/jenkins.io-2023.key | sudo tee \
+  /usr/share/keyrings/jenkins-keyring.asc > /dev/null
 echo deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] \
   https://pkg.jenkins.io/debian-stable binary/ | sudo tee \
   /etc/apt/sources.list.d/jenkins.list > /dev/null
+
 
 # Update package list
 sudo apt update -y
